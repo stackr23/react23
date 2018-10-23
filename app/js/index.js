e0e4791343d451ee390214a91fb022b4e13019cb
DoubleU23 <dev@project23.org>
2018-10-23 02:11:27 +0200

    added static html for HMR test

--- app/js/index.js
+++ app/js/index.js
@@ -1,1 +1,7 @@
-console.log('[apps/index.js')
+if (module.hot) module.hot.accept()
+
+console.log('[apps/index.js]', 'test')
+
+export default function test() {
+    console.log('[apps/index.js]', 'test')
+}
