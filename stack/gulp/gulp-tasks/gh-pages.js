c6523f67e9ab00a0e17fcb241682da32d785b1a2
DoubleU23 <dev@project23.org>
2018-10-28 13:14:29 +0100

    C:/Program Files/Git/build is now a worktree - reignored build

---
+++ stack/gulp/gulp-tasks/gh-pages.js
@@ -0,0 +1,20 @@
+// import gulp         from 'gulp'
+// import gulpGhPages  from 'gulp-gh-pages'
+
+// const {paths: {build: buildPath}} = require('config').default
+
+// TBD: run sh script "deploy_to_github"
+// (cd dist; git add .)
+// (cd dist; git commit -m "Build output as of $(git log '--format=format:%H' master -1)"
+// git push origin gh-pages
+//
+
+// const deployToGithub = done =>
+//     gulp.src(buildPath + '/**/*')
+//         .pipe(gulpGhPages({
+//             force: true
+//         }))
+
+// gulp.task('gh-pages', gulp.series(deployToGithub))
+
+// export default deployToGithub
