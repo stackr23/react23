b42615281739eb2ac4de829ef0634dcac0181071
DoubleU23 <dev@project23.org>
2018-11-12 15:33:23 +0100

    :construction_worker: added gh-pages/init and /deploy to travis script

--- stack/scripts/gh-pages/deploy.sh
+++ stack/scripts/gh-pages/deploy.sh
@@ -1,11 +1,9 @@
 #!/bin/bash
 
-(npm run build && \
-cd build && \
+(cd build && \
 git add --all && \
 git commit -m ":rocket: deploy build to gh-pages" && \
 git push origin gh-pages) || \
 # Your branch is up to date with 'origin/gh-pages'.
 # nothing to commit, working tree clean
-# - or build failed
 echo "[react23] Build new version and try again."
