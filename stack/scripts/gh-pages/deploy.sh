7597796f61b250c4194f0bcf47136596a3775015
DoubleU23 <dev@project23.org>
2018-11-12 13:54:39 +0100

    :children_crossing: added "npm run build" to gh-pages/deploy

--- stack/scripts/gh-pages/deploy.sh
+++ stack/scripts/gh-pages/deploy.sh
@@ -1,9 +1,11 @@
 #!/bin/bash
 
-(cd build && \
+(npm run build && \
+cd build && \
 git add --all && \
 git commit -m ":rocket: deploy build to gh-pages" && \
 git push origin gh-pages) || \
 # Your branch is up to date with 'origin/gh-pages'.
 # nothing to commit, working tree clean
+# - or build failed
 echo "[react23] Build new version and try again."
