9a17f13190524cfc00af998aea2ec44c2119ef3e
DoubleU23 <dev@project23.org>
2018-11-12 12:25:10 +0100

    :bug: fixxed gh-pages/init

--- stack/scripts/gh-pages/init.sh
+++ stack/scripts/gh-pages/init.sh
@@ -1,7 +1,9 @@
 #!/bin/bash
 
-(git worktree remove build && \
-rm build/ -rf && \
-git worktree add build gh-pages) || \
+git worktree remove build
+
+rm build/ -rf
+
+git worktree add build gh-pages || \
 # on error
 echo "[react23] Could not initiate worktree on /build."
