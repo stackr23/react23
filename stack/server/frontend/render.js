126ef6ac55e72b60da6ff9d75d1a5cf62cbc5d95
Stefan Friedl <friedl@abss.at>
2018-11-13 13:26:47 +0100

    :wrench: 755 file permissions

--- stack/server/frontend/render.js
+++ stack/server/frontend/render.js
@@ -14,8 +14,8 @@ import ip                               from 'ip'
 // refactor                            : use bluebird as polyfill on entrypoint(s)
 // import Promise                          from 'bluebird'
 // APP FILES
-import getBuiltIndex                    from '../../utils/getBuiltIndex'
-import getBuildFileNames                from '../../utils/getBuildFileNames'
+import getBuiltIndex                    from '../../utils/getBuiltIndex.js'
+import getBuildFileNames                from '../../utils/getBuildFileNames.js'
 
 import {StaticRouter}                   from 'react-router'
 // import initialState                     from '../../../app/js/stores/initialState.js'
@@ -40,7 +40,7 @@ export default function render (req, res, next) {
     let appJS, appCSS // set per NODE_ENV
     const {appJS: appJsFilename, appCSS: appCssFilename} = getBuildFileNames()
 
-    if (isProduction) {
+    if (isProduction || process.env.APP_BUILD_STATIC) {
         // TBD: npm run start --production
         appJS   = `/build/${appJsFilename}`
         appCSS  = `build/${appCssFilename}`
@@ -51,11 +51,23 @@ export default function render (req, res, next) {
 
     const indexHtml = getBuiltIndex({appCSS, appJS})
 
-    const html = '<!DOCTYPE html><!-- req.url:' + req.url + '-->\n' + indexHtml.replace(
+    const html = '<!DOCTYPE html>\n' + indexHtml.replace(
         '<div id="app"></div>', `<div id="app">${appHtml}</div>`
     )
 
     res.send(html)
+}
+
+const renderPage = ({path, stores, context = {}}) => {
+    const appHtml   = renderToStaticMarkup(
+        <Provider {...stores}>
+            <StaticRouter location={path} context={context}>
+                <Layout  />
+            </StaticRouter>
+        </Provider>
+    )
+
+    return appHtml
 }
 
 // TBD: fetchComponentDataAsync
@@ -82,35 +94,3 @@ export default function render (req, res, next) {
 // const body      = /<body>(.+)<\/body>/.exec(indexHtml)
 // console.log('indexHtml', indexHtml)
 // console.log('parsedBody', body)
-
-const renderPage = ({path, stores, context = {}}) => {
-    const appHtml   = renderToStaticMarkup(
-        <Provider {...stores}>
-            <StaticRouter location={path} context={context}>
-                <Layout  />
-            </StaticRouter>
-        </Provider>
-    )
-
-    return appHtml
-
-    // router.dispatch({ ...req, context }).then((component, state) => {
-    //     res.status(state.statusCode).send(indexHtml)
-    // }).catch(next)
-
-    // return '<!DOCTYPE html>' + renderToStaticMarkup(
-    //     <html>
-    //         <head />
-    //         <body>
-    //             <link href="https://fonts.googleapis.com/icon?family=Material+Icons" rel="stylesheet" />
-    //             <div id="app" />
-    //             <script type="text/javascript" src={appJS} />
-    //             {// in dev, styles are handled by style-loader
-    //                 // which directly injects them into the DOM
-    //                 !isDevelopment &&
-    //                 <link rel="stylesheet" href={styleSrc} name="appStyle" />}
-    //         </body>
-    //     </html>
-    // )
-}
-
---
+++ test/nativeCss.js
@@ -0,0 +1,13 @@
+{
+  "styles": {
+    "palette_primary": {
+      "main": "#1a237e"
+    },
+    "palette_secondary_test": {
+      "mainColor": "#ffb300"
+    },
+    "overrides__MuiButton__root": {
+      "color": "#fff"
+    }
+  }
+}
