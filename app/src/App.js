319b43b7fec954df3b46fc898429de888cf09431
DoubleU23 <dev@project23.org>
2019-08-01 20:07:15 +0200

    re-add compiled routes as Layout's children

--- app/src/App.js
+++ app/src/App.js
@@ -1,15 +1,14 @@
 import React from 'react'
-// import {Route} from 'react-router-dom'
 import Helmet from 'react-helmet'
 import PropTypes from 'prop-types'
 
 import {observer, inject} from 'mobx-react'
-// import stores from '../stores'
+import stores from '../stores'
 
 import Layout from './Layout'
-/* import createRoutes from '../routes/index'
+import createRoutes from '../routes/index'
 
-const routesCompiled = createRoutes(stores) */
+const routesCompiled = createRoutes(stores)
 
 @inject('router')
 @observer
@@ -19,28 +18,24 @@ class App extends React.Component {
     }
 
     render() {
-        /*       const {
-        router: {
-          location: {pathname}
-        }
-      } = this.props */
-        // console.log('global config', global.CONFIG)
+        const {
+            router: {location: {pathname}}
+        } = this.props
 
-        //   const route = routesCompiled.filter((r) => r.props.path === pathname)[0]
-        //   const title = route.props.meta.title
-        //   const description = route.props.meta.description || ''
+        // extract actual route's meta data
+        const route = routesCompiled.filter((r) => r.props.path === pathname)[0]
+        const title = route.props.meta.title
+        const description = route.props.meta.description || ''
 
         return (
             <div id="app">
                 <Helmet>
                     <meta charSet="utf-8" />
-                    <title>[React23] - {"title"}</title>
-                    <meta name="description" content={"description"} />
+                    <title>[React23] - {title}</title>
+                    <meta name="description" content={description} />
                     <link rel="canonical" href="http://mysite.com/example" />
                 </Helmet>
-                <Layout />
-
-                {/* <Layout>{routesCompiled}</Layout> */}
+                <Layout>{routesCompiled}</Layout>
             </div>
         )
     }
