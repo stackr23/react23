6220fa5cffe7d56117cf70461517bc819fc2023f
DoubleU23 <dev@project23.org>
2019-08-11 22:46:21 +0200

    re-add routes meta props

--- app/src/App.js
+++ app/src/App.js
@@ -8,37 +8,45 @@ import stores from '../stores'
 import Layout from './Layout'
 import createRoutes from '../routes/index'
 
+import {withRouter} from 'react-router'
+import {Switch} from 'react-router-dom'
+
 const routesCompiled = createRoutes(stores)
 
-@observer
-@inject('router')
+// @observer
+// @inject('router')
+@withRouter
 class App extends React.Component {
     static propTypes = {
-        router: PropTypes.object.isRequired
+        location: PropTypes.shape({
+            pathname: PropTypes.string.isRequired
+        }),
     }
 
     render() {
-        console.log(this.props.router)
-        console.log('this.props.router.history :', this.props.router.history)
+        const {
+            location
+        } = this.props
 
-        // const {
-        //     router: {history: {location}}
-        // } = this.props
+        // extract actual route's meta data
+        const route = routesCompiled.filter((r) => r.props.path === location.pathname)[0]
 
-        // // extract actual route's meta data
-        // const route = routesCompiled.filter((r) => r.props.path === location.pathname)[0]
-        // const title = route.props.meta.title
-        // const description = route.props.meta.description || ''
+        const title = route.props.meta.title
+        const description = route.props.meta.description || ''
 
         return (
             <div id="app">
                 <Helmet>
                     <meta charSet="utf-8" />
-                    <title>[React23] - {'title'}</title>
-                    <meta name="description" content={'description'} />
+                    <title>[React23] - {title}</title>
+                    <meta name="description" content={description} />
                     <link rel="canonical" href="http://mysite.com/example" />
                 </Helmet>
-                <Layout>{routesCompiled}</Layout>
+                <Layout>
+                    <Switch>
+                        {routesCompiled}
+                    </Switch>
+                </Layout>
             </div>
         )
     }
