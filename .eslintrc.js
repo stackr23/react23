c2e91c6804020fe090eaa618ae0c81ac6431eb35
viewar_sf <dev@project23.org>
2019-09-09 20:42:56 +0200

    lint: ignore "no-extraneous-import"

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,12 +1,13 @@
 const config = {
   "root": true,
-  "extends": ["viewar/env/react"],
+  "extends": ["viewar/env/react"], // "stackr23/node"],
   plugins: ['jsx'],
   "rules": {
     semi: ['error', 'never'],
     'node-unpublished': 0,
     "jsx/mark-used-vars": 1,
-    'node/no-missing-import': 0
+    'node/no-extraneous-import': 0,
+    'node/no-extraneous-require': 0
   }
 }
 
