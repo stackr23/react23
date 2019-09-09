797afaa20335bb4cef023cb5dc90c555e21ffc3c
viewar_sf <dev@project23.org>
2019-09-09 18:22:21 +0200

    use /app as module root

--- webpack.config.resolve.js
+++ webpack.config.resolve.js
@@ -1,9 +1,8 @@
 const { paths } = require('config').default
 
 module.exports = {
-  root: 'app',
   resolve: {
     extensions: [ '.js', '.jsx', '.json', '*' ],
-    modules: [ 'src', 'app', paths.nodeModules ]
+    modules: [ '.', 'src', paths.nodeModules ]
   }
 }
